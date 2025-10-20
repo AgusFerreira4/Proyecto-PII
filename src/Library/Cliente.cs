5f711b5559dde49c8473edbe68deb5331715c0bf
Ignacio Rodriguez <ignaciorodriguezsantos5@gmail.com>
2025-10-20 10:40:49 -0300

    usuario

--- src/Library/Cliente.cs
+++ src/Library/Cliente.cs
@@ -1,19 +1,14 @@
-﻿using System.Security.AccessControl;
+﻿using System;
+using System.Collections.Generic;
+using System.Security.AccessControl;
 
 namespace Library;
 
 public class Cliente : Persona
 {
-/*+ Telefono : string
-+ Genero : string
-    + FechaDeNacimienta : string 
-    + Etiquetas : List<string> 
-    + UsuarioAsignado : Usuario
-    - ListaInteracciones : GenericContainer<Interaccion>
-    */
     public string Telefono { get; set; }
     public string Genero { get; set; }
-    public string FechaDeNacimiento { get; set; }
+    public DateTime FechaDeNacimiento { get; set; }
     public List<string> Etiquetas { get; set; }
     public Usuario UsuarioAsignado { get; set; }
     private GenericContainer<Interaccion> ListaInteraccion { get; set; }
